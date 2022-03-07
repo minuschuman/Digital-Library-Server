8b9d690df6782f55e3fcc6cc826cded306ae0123
chumanstha <chumanashrestha@gmail.com>
2022-03-08 01:08:08 +0545

    final port and url

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = process.env.LOCAL_ADDRESS || 5000;
 
 const server = http.createServer((req, res) => {
@@ -10,11 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-// const { PORT = 5000, LOCAL_ADDRESS = "0.0.0.0 || 127.0.0.1" } = process.env;
-// server.listen(PORT, LOCAL_ADDRESS, () => {
-//   const address = server.address();
-//   console.log("server listening at", address);
-// });
