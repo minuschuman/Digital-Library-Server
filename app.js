2392223abe9f5aa3205ee1520fc3f729f1d14c8d
chumanstha <chumanashrestha@gmail.com>
2022-03-08 01:11:45 +0545

    roll back

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require("http");
-
-const hostname = "0.0.0.0";
-const port = process.env.LOCAL_ADDRESS || 5000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const { PORT = 5000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log("server listening at", address);
 });
