941462402f4d867391ffcee7a1b4f263c5154125
chumanstha <chumanashrestha@gmail.com>
2022-03-08 00:54:06 +0545

    change server host

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const { PORT = 5000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log("server listening at", address);
 });
