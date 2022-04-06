f4f238df22fad9fe503b5218820cff026acb3031
chumanstha <chumanashrestha@gmail.com>
2022-04-06 16:19:42 +0545

    add schema of book

---
+++ api/models/book.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { required } = require("nodemon/lib/config");
+
+const BookSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  bookFile: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Book", BookSchema);
