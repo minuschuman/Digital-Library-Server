563578bb04fce489810dc9784a03a744757278ea
chumanstha <chumanashrestha@gmail.com>
2022-04-06 16:21:24 +0545

    update route and models of file

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const { PORT = 5000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
-server.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = server.address();
-  console.log("server listening at", address);
+
+
+const bookRoute = require("./api/routes/books");
+const orderRoute = require("./api/routes/orders");
+const userRoute = require("./api/routes/user");
+
+app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
 });
+
+app.use("/books", bookRoute);
+app.use("/orders", orderRoute);
+app.use("/user", userRoute);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+const dbURI = process.env.DB_CONNECTION;
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    app.listen(PORT, LOCAL_ADDRESS, () => {
+      console.log(`server listening at http://${LOCAL_ADDRESS}:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
