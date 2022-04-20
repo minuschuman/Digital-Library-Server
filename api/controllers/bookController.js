9e87e155e8fa4401f746b6c31028081d323132d9
chumanstha <chumanashrestha@gmail.com>
2022-04-20 20:56:36 +0545

    separate logics from route

---
+++ api/controllers/bookController.js
@@ -0,0 +1,151 @@
+const Book = require("../models/book");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+exports.books_get_all = (req, res, next) => {
+  Book.find()
+    .select("name isbn author price bookFile _id")
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        books: docs.map((doc) => {
+          return {
+            name: doc.name,
+            isbn: doc.isbn,
+            author: doc.author,
+            price: doc.price,
+            bookFile: "/" + doc.bookFile,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "/books/" + doc._id,
+            },
+          };
+        }),
+      };
+      if (docs) {
+        res.status(200).json(response);
+      } else {
+        res.status(404).json({
+          message: "Data Not Found",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
+
+exports.books_file_new = upload.single("bookFile");
+
+exports.books_add_new = (req, res, next) => {
+  // console.log(req.file);
+  const book = new Book({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    category: req.body.category,
+    isbn: req.body.isbn,
+    author: req.body.author,
+    price: req.body.price,
+    edition: req.body.edition,
+    publication: req.body.publication,
+    bookFile: req.file.path,
+  });
+  book
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Created successfully",
+        createdBook: {
+          name: result.name,
+          isbn: result.isbn,
+          author: result.author,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "/books/" + result._id,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      if (err.name == "ValidationError") {
+        console.error("Error Validating!", err.errors);
+        res.status(422).json(err.errors);
+      } else {
+        res.status(500).json({
+          error: err,
+        });
+      }
+    });
+};
+
+exports.books_get_single = (req, res, next) => {
+  const id = req.params.bookId;
+  Book.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "Entry not found",
+        });
+      }
+    })
+    .catch((err) => {
+      // console.log("hehe"+err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
+
+exports.books_update = (req, res, next) => {
+  const id = req.params.bookId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Book.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
+
+exports.books_remove = (req, res, next) => {
+  const id = req.params.bookId;
+  Book.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
--- api/routes/books.js
+++ api/routes/books.js
@@ -1,147 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+const BookController = require("../controllers/bookController");
 
-const Book = require("../models/book");
+router.get("/", BookController.books_get_all);
+router.post(
+  "/",
+  checkAuth,
+  BookController.books_file_new,
+  BookController.books_add_new
+);
+router.get("/:bookId", BookController.books_get_single);
+router.patch("/:bookId", checkAuth, BookController.books_update);
+router.delete("/:bookId", checkAuth, BookController.books_remove);
 
-router.get("/", (req, res, next) => {
-  Book.find()
-    .select("name isbn author price bookFile _id")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        books: docs.map((doc) => {
-          return {
-            name: doc.name,
-            isbn: doc.isbn,
-            author: doc.author,
-            price: doc.price,
-            bookFile: "/" + doc.bookFile,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "/books/" + doc._id,
-            },
-          };
-        }),
-      };
-      if (docs) {
-        res.status(200).json(response);
-      } else {
-        res.status(404).json({
-          message: "Data Not Found",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.post("/", checkAuth, upload.single("bookFile"), (req, res, next) => {
-  console.log(req.file);
-  const book = new Book({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    isbn: req.body.isbn,
-    author: req.body.author,
-    price: req.body.price,
-    bookFile: req.file.path,
-  });
-  book
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "Created successfully",
-        createdBook: {
-          name: result.name,
-          isbn: result.isbn,
-          author: result.author,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: "GET",
-            url: "/books/" + result._id,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.get("/:bookId", (req, res, next) => {
-  const id = req.params.bookId;
-  Book.findById(id)
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        res.status(404).json({
-          message: "Entry not found",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.patch("/:bookId", checkAuth, (req, res, next) => {
-  const id = req.params.bookId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  Book.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.delete("/:bookId", checkAuth, (req, res, next) => {
-  const id = req.params.bookId;
-  Book.remove({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
 module.exports = router;
