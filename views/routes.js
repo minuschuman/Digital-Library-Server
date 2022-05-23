43984224c27287e94e1f367395eb97fa0a106152
chumanstha <chumanashrestha@gmail.com>
2022-05-23 10:47:30 +0545

    code optimize by movinig code to controller

--- views/routes.js
+++ views/routes.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const router = express.Router();
 // const checkAuth = require("../middleware/check-auth");
@@ -12,4 +13,34 @@ router.get("/dash", function (req, res) {
   res.render("dash");
 });
 
+router.get("/dashboard", function (req, res) {
+  res.render("dashboard");
+});
+
+//route for book
+router.get("/book", function (req, res) {
+  res.render("book/index");
+});
+
+router.get("/book/add", function (req, res) {
+  res.render("book/add");
+});
+
+router.get("/book/edit", function (req, res) {
+  res.render("/book/edit");
+});
+
+router.get("/book/category", function (req, res) {
+  res.render("/book/category");
+});
+
+router.get("/user", function (req, res) {
+  res.render("user/index");
+});
+router.get("/user/edit", function (req, res) {
+  res.render("user/edit");
+});
+router.get("/user/book", function (req, res) {
+  res.render("user/book");
+});
 module.exports = router;
