3a5a8f6a789fe0a8ed27230d4a728cac465533f2
minuschuman <sthachuman@gmail.com>
2022-06-09 22:59:31 +0545

    get new data of user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const checkAuth = require("../middleware/check-auth");
 
 const UsersConontroller = require("../controllers/user");
 
 router.post("/signup", UsersConontroller.user_singup);
 router.post("/login", UsersConontroller.user_login);
 router.delete("/:userId", UsersConontroller.user_delete);
+
+router.get("/", checkAuth, UsersConontroller.users_get_all);
+router.get("/:userId", checkAuth, UsersConontroller.user_get_single);
+router.patch("/:userId", checkAuth, UsersConontroller.user_update);
 
 module.exports = router;
