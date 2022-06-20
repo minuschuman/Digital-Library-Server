6b61ba5cee04f0ff24a5452d534c5c91079f96b8
minuschuman <sthachuman@gmail.com>
2022-06-21 00:08:43 +0545

    add pending route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const checkUser = require("../middleware/check-user");
 
-
 const OrdersConontroller = require("../controllers/order");
 
 router.get("/", checkUser, OrdersConontroller.orders_get_all);
+router.get("/pending", checkUser, OrdersConontroller.orders_pending);
 router.post("/", checkUser, OrdersConontroller.orders_add_new);
 router.get("/:orderId", checkUser, OrdersConontroller.orders_get_single);
 router.delete("/:orderId", checkUser, OrdersConontroller.orders_remove);
