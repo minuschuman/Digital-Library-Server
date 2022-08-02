d61175c8c72c54947f43b6fba93e4cce04853f98
minuschuman <sthachuman@gmail.com>
2022-08-03 05:38:45 +0545

    update route file

--- views/routes.js
+++ views/routes.js
@@ -9,38 +9,38 @@ router.get("/", function (req, res) {
   res.render("index");
 });
 
-router.get("/dash", function (req, res) {
-  res.render("dash");
-});
+// router.get("/dash", function (req, res) {
+//   res.render("dash");
+// });
 
-router.get("/dashboard", function (req, res) {
-  res.render("dashboard");
-});
+// router.get("/dashboard", function (req, res) {
+//   res.render("dashboard");
+// });
 
-//route for book
-router.get("/book", function (req, res) {
-  res.render("book/index");
-});
+// //route for book
+// router.get("/book", function (req, res) {
+//   res.render("book/index");
+// });
 
-router.get("/book/add", function (req, res) {
-  res.render("book/add");
-});
+// router.get("/book/add", function (req, res) {
+//   res.render("book/add");
+// });
 
-router.get("/book/edit", function (req, res) {
-  res.render("/book/edit");
-});
+// router.get("/book/edit", function (req, res) {
+//   res.render("/book/edit");
+// });
 
-router.get("/book/category", function (req, res) {
-  res.render("/book/category");
-});
+// router.get("/book/category", function (req, res) {
+//   res.render("/book/category");
+// });
 
-router.get("/user", function (req, res) {
-  res.render("user/index");
-});
-router.get("/user/edit", function (req, res) {
-  res.render("user/edit");
-});
-router.get("/user/book", function (req, res) {
-  res.render("user/book");
-});
+// router.get("/user", function (req, res) {
+//   res.render("user/index");
+// });
+// router.get("/user/edit", function (req, res) {
+//   res.render("user/edit");
+// });
+// router.get("/user/book", function (req, res) {
+//   res.render("user/book");
+// });
 module.exports = router;
