4c0a51afacd5dbb1d8f18d385f4791cdfbd16bdc
minuschuman <sthachuman@gmail.com>
2022-08-03 05:02:08 +0545

    add differernt status and route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,10 +4,12 @@ const checkUser = require("../middleware/check-user");
 
 const OrdersConontroller = require("../controllers/order");
 
-router.get("/", checkUser, OrdersConontroller.orders_get_all);
 router.get("/pending", checkUser, OrdersConontroller.orders_pending);
+router.get("/check", checkUser,OrdersConontroller.return_true);
+router.get("/", checkUser, OrdersConontroller.orders_get_all);
 router.post("/", checkUser, OrdersConontroller.orders_add_new);
 router.get("/:orderId", checkUser, OrdersConontroller.orders_get_single);
+router.get("/:status/:UserId", checkUser, OrdersConontroller.orders_getAll_personWise);
 router.delete("/:orderId", checkUser, OrdersConontroller.orders_remove);
 
 module.exports = router;
