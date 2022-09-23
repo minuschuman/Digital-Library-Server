650067c821fe86da783d39132b4c44b992d49b33
minuschuman <sthachuman@gmail.com>
2022-09-23 13:57:44 +0545

    add password changer controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -198,3 +198,84 @@ exports.user_get_single = (req, res, next) => {
       });
     });
 };
+
+exports.user_password = (req, res, next) => {
+  const id = req.params.userId;
+  const newPass = req.body.newPass;
+  const oldPass = req.body.oldPass;
+  if (!oldPass) {
+    return res.status(401).json({
+      error: "Provide oldPass",
+    });
+  }
+
+  if (!newPass) {
+    return res.status(401).json({
+      error: "Provide new password",
+    });
+  }
+
+  if (oldPass == newPass) {
+    return res.status(401).json({
+      error: "old pass can't be new password",
+    });
+  }
+
+  User.findById(id)
+    .exec()
+    .then((user) => {
+      bcrypt.compare(oldPass, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Somting went wrong",
+            err: err.message,
+          });
+        }
+        if (!result) {
+          return res.status(401).json({
+            message: "Old password doesn't match",
+          });
+        }
+
+        bcrypt.hash(newPass, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            User.findOneAndUpdate(
+              { _id: id },
+              { password: hash },
+              (err, upResult) => {
+                if (err) {
+                  res.status(401).json({
+                    message: "Somting went wrong",
+                    err: err,
+                  });
+                }
+                if (upResult) {
+                  res.status(201).json({
+                    message: "Password updated",
+                  });
+                }
+              }
+            );
+          }
+        });
+      });
+
+      // .save()
+      // .then((resust) => {
+      //   console.log(resust);
+      //   res.status(201).json({
+      //     message: "Password updated",
+      //   });
+      // })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
